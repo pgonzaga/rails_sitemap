41f0ada47f54bd9511ded724f77224e3a2e2bc9b
Pablo Gonzaga <pgonzaga@Pablos-MacBook-Air.local>
2016-09-23 18:51:18 -0300

    Add custom configuration

--- lib/rails_sitemap/engine.rb
+++ lib/rails_sitemap/engine.rb
@@ -1,5 +1,20 @@
 module RailsSitemap
   class Engine < ::Rails::Engine
     isolate_namespace RailsSitemap
+
+    initializer 'rails_sitemap', before: :load_config_initializers do |app|
+      Rails.application.routes.append do
+        mount RailsSitemap::Engine => '/'
+      end
+    end
+  end
+
+  class << self
+    mattr_accessor :models_for_sitemap
+    self.models_for_sitemap = []
+  end
+
+  def self.setup(&block)
+    yield self
   end
 end
