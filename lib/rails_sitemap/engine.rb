84ab8ad50615fbb364eca5b08ce12c8571a28a43
Pablo Gonzaga <pgonzaga@Pablos-MacBook-Air.local>
2016-09-26 14:58:22 -0300

    Add updated at, frequency and last modification configuration options

--- lib/rails_sitemap/engine.rb
+++ lib/rails_sitemap/engine.rb
@@ -10,8 +10,17 @@ module RailsSitemap
   end
 
   class << self
-    mattr_accessor :models_for_sitemap
+    mattr_accessor :models_for_sitemap,
+                   :updated_at,
+                   :priority,
+                   :update_frequency_for_app,
+                   :update_frequency_for_models
+
     self.models_for_sitemap = []
+    self.updated_at = DateTime.now.to_s
+    self.priority = 0.5
+    self.update_frequency_for_app = 'always'
+    self.update_frequency_for_models = 'weekly'
   end
 
   def self.setup(&block)
--- test/dummy/config/initializers/rails_sitemap.rb
+++ test/dummy/config/initializers/rails_sitemap.rb
@@ -1,3 +1,7 @@
 RailsSitemap.setup do |config|
   config.models_for_sitemap = %w(Article)
+  config.updated_at = '2016-09-22T18:11:05-03:00'
+  config.priority = 0.5
+  config.update_frequency_for_app = 'always'
+  config.update_frequency_for_models = 'weekly'
 end
