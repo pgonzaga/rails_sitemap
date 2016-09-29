07ebc675f5395676d192c2825fef9b7cd997337a
Pablo Gonzaga <pgonzaga@Pablos-MacBook-Air.local>
2016-09-29 14:05:43 -0300

    Move sitemap.xml endpoint to sitemap_index.xml

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ RailsSitemap::Engine.routes.draw do
   get 'pages-sitemap' => 'pages#index'
   get 'attachment-sitemap' => 'attachment#index'
   get 'geo-sitemap' => 'geo#index'
-  get 'sitemap' => 'sitemaps#index'
+  get 'sitemap_index' => 'sitemaps#index'
 
   resources :locations, only: :index
 end
