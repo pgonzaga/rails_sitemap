e59aa8a2b9bba4b560e92ddfa9f5dd805d6cb03e
Pablo Gonzaga <pgonzaga@Pablos-MacBook-Air.local>
2016-10-03 11:28:54 -0300

    Fix add assets host to attachment sitemap

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -36,6 +36,7 @@ Rails.application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
+  config.action_controller.asset_host = 'http://www.example.com/'
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
